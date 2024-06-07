915a88ee6c9162338986ab418fe3fd2c2c064dfe
Femreyilmaz <emreyilmaz66666@gmail.com>
2024-06-07 12:28:47 +0300

    Add project files.

---
+++ CQRSProject/Controllers/ProductContoller.cs
@@ -0,0 +1,21 @@
+﻿using CQRSProject.CQRS.Handlers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRSProject.Controllers
+{
+    public class ProductContoller : Controller
+    {
+        private readonly GetProductQueryHandler _getProductQueryHandler;
+
+        public ProductContoller(GetProductQueryHandler getProductQueryHandler)
+        {
+            _getProductQueryHandler = getProductQueryHandler;
+        }
+
+        public IActionResult ProductList()
+        {
+            var values = _getProductQueryHandler.Handle();
+            return View(values);
+        }
+    }
+}
---
+++ CQRSProject/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CQRSProject.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-17Q1DFV\\SQLEXPRESS; initial Catalog = DbCqrs; integrated Security = true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
